21112854e2928ba122718332e92123f1fb7e9282
phap1802 <phap1802@gmail.com>
2025-03-06 08:07:44 -0300

    Finalizando NAVBAR - Ajustando Responsividade

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -1,32 +1,5 @@
 
-/* BOTAO SWITCH THEMA ANIMAÇÃO e FUNCIONALIDADE */
 
-const btnSwitch = document.querySelector('#btnSwitch');
-const btnCircle = document.querySelector('#btnCircle');
-
-// Verifica se já existe um tema salvo no localStorage
-//Tenta pegar o valor da chave 'theme' no localStorage
-const savedTheme = localStorage.getItem("theme"); 
-
-//verifica se o valor de saveTheme é igual a "light"
-if (savedTheme === "light") {
-    //se verdadeiro, entao adiciona as classes "lightTheme" e "move" nos elementos.
-    document.documentElement.classList.add("lightTheme");
-    btnSwitch.classList.add("move");
-    btnCircle.classList.add("move");
-}
-
-btnSwitch.addEventListener("click", () => {
-    btnSwitch.classList.toggle("move");
-    btnCircle.classList.toggle("move");
-    document.documentElement.classList.toggle("lightTheme");
-
-    // Verifica o tema atual e salva no localStorage
-    //verifica se contem o "lightTheme" na classe do elemento.
-    const isLight = document.documentElement.classList.contains("lightTheme");
-    //Se tiver a classe "lightTheme" no elemento, salva "light" no localStorage, senão salva "dark".
-    localStorage.setItem("theme", isLight ? "light" : "dark");
-})
 
     
 
