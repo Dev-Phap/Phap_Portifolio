b231ef499b6339493bc7e72dc5ad704bdec6b155
phap1802 <phap1802@gmail.com>
2025-03-26 13:22:34 -0300

    SectionAbout - Feito Caxa da Seção e Atribuido animação de expandir e recolher

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,34 @@ btnSwitch.addEventListener("click", () => {
 
 
 
+/* Botao para abrir a seção e recolher */
+
+const btnExpand = document.querySelectorAll(".btn_arrowImage");
+
+//para cada botão expansivo vai adicionar o evento de click
+btnExpand.forEach((btn) => {
+
+ btn.addEventListener('click', () =>{
+        //no enveto de click sera expandido o conteudo que se refere ao elemento pai do botão que foi clicado.
+
+        //o metodo closest procura o ancestro mais proximo que tenha a classe "boxSection" 
+        //Apos procurar o elemento pai com closest usamos query selector que pega o primeiro filho com a classe testeConteudo
+        const conteudo = btn.closest('.boxSection').querySelector('.content');
+        conteudo.classList.toggle('active');
+        
+
+        //aqui verifica se o botão de seta tem incluso "baixo" no src da tag img, e caso verdadeiro muda de "baixo" para "cima". caso falso muda de "cima" para  "baixo"
+        if (btn.src.includes('baixo')) {
+            btn.src = btn.src.replace('baixo', 'cima');
+        } else {
+            btn.src = btn.src.replace('cima', 'baixo');
+        }
+ });
+});
+
+
+
+
 
 /* Aplicando Efeitos Globais  */
 
@@ -69,3 +97,4 @@ function aplicarEfeitoEsmaecer() {
         });
     }, 1300); // Delay antes de iniciar o esmaecimento (depois da digitação)
 }
+
