e94d333e3e07b4aee5814528efe74799ecf3b628
phap1802 <phap1802@gmail.com>
2025-04-24 12:00:32 -0300

    Fix Bug <hr> invisible, and refactore class and scripts subsScetions

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,6 +82,41 @@ btnExpand.forEach((btn) => {
 
 
 
+
+
+/* Botão para abri e fechar subs seções */
+
+
+const btnExpandSubSection = document.querySelectorAll(".btn_arrowImage_subBoxSection");
+
+//para cada botão expansivo vai adicionar o evento de click
+btnExpandSubSection.forEach((btn) => {
+
+ btn.addEventListener('click', () =>{
+        //no enveto de click sera expandido o conteudo que se refere ao elemento pai do botão que foi clicado.
+
+        //o metodo closest procura o ancestro mais proximo que tenha a classe "boxSection" 
+        //Apos procurar o elemento pai com closest usamos query selector que pega o primeiro filho com a classe testeConteudo
+        const conteudo = btn.closest('.container_subSection').querySelector('.content_subSection');
+        conteudo.classList.toggle('active');
+        
+        
+        //aqui verifica se o botão de seta tem incluso "baixo" no src da tag img, e caso verdadeiro muda de "baixo" para "cima". caso falso muda de "cima" para  "baixo"
+        if (btn.src.includes('baixo')) {
+            btn.src = btn.src.replace('baixo', 'cima');
+        } else {
+            btn.src = btn.src.replace('cima', 'baixo');
+        }
+ });
+});
+
+
+
+
+
+
+
+
 /* Aplicando Efeitos Globais  */
 
 
