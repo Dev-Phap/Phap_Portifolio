24665495e5315481f749a05c321c223dfc2a323e
phap1802 <phap1802@gmail.com>
2025-04-25 15:06:33 -0300

    create the card content of the subscetion and add function open/closed description card

---
+++ assets/js/sectionTrajetoria.js
@@ -0,0 +1,49 @@
+
+
+const btnSeeMore = document.querySelectorAll('.btn_arrowImage_seeMore');
+
+btnSeeMore.forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+        const currentItem = btn.closest('.content_list_trajetoria')
+        const currentDescription = currentItem.querySelector('.container_description_trajetoria');
+        
+
+        const allItems = document.querySelectorAll('.content_list_trajetoria');
+
+        
+
+
+        // preciso de outra função para verificar os outros itens para ver se estao fechados. 
+
+        allItems.forEach( (item) => {
+
+            //verifico se o item é diferente do item atual se verdadeiro entra no bloco
+            if ( item !== currentItem) {
+                //pego outra descrição e outros botão pois todos os items que entrar no bloco sao diferente da descrição atual. 
+                const otherDescription = item.querySelector('.container_description_trajetoria');
+
+                const otherBtn = item.querySelector('.btn_arrowImage_seeMore');
+
+                //fecha os outros Descrição abertos removendo a classe active
+                otherDescription.classList.remove('active');
+                
+                //muda posição da seta para ao fechar as outras descrição
+                if (otherBtn.src.includes('cima')){
+                    otherBtn.src = otherBtn.src.replace('cima', 'baixo');
+                }
+            }
+        })
+
+        //ativa o descição clicada atual. 
+        currentDescription.classList.toggle('active');
+        //muda a posição da seta para fechar 
+        if (btn.src.includes('baixo')) {
+            btn.src = btn.src.replace('baixo', 'cima');
+        } else {
+            btn.src = btn.src.replace('cima', 'baixo');
+        }
+
+    })
+})
+
